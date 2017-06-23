e8886fbeb89fdfa72aa16ca21d9b2cff3b8fb010
Patel <212066255@logon.ds.ge.com>
2017-06-22 22:01:00 -0500

    first commit

---
+++ test3/src/main/java/com/mycomp/mydept/test3/Employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mydept.test3;
+
+/**
+ *
+ * @author 212066255
+ */
+public class Employee {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + '}';
+    }
+    
+    
+}
